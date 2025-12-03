e16c3a0e4a3de7d1af84d9595bfe97fde06a28e4
dutch <dutchiono@gmail.com>
2025-12-02 23:53:27 -0500

    last one1

--- apps/web/mdx-components.tsx
+++
@@ -1,7 +0,0 @@
-
-import type { MDXComponents } from 'mdx/types';
-import { useMDXComponents as useNextraMDXComponents } from 'nextra-theme-docs';
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return useNextraMDXComponents(components);
-}

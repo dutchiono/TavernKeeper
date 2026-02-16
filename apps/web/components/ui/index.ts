5a4b16e265c652a25691581544f5b435f721809b
dutchiono <86275975+dutchiono@users.noreply.github.com>
2026-02-16 16:54:59 -0500

    Add UI component library index

---
+++ apps/web/components/ui/index.ts
@@ -0,0 +1,4 @@
+// UI Component Library
+export { ToastProvider, useToast } from './Toast';
+export { LoadingSpinner, LoadingOverlay, LoadingInline } from './LoadingSpinner';
+export { ErrorBoundary } from './ErrorBoundary';
